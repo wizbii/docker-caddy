2eeb02d1f219a33965dc9dacfe80195904d596d5
Florent Baldino <florent.baldino@wizbii.com>
2019-02-21 09:54:43 +0100

    Import the real 'cors' plugin instead of the library

--- plugins.go
+++ plugins.go
@@ -2,7 +2,7 @@ package caddyhttp
 
 import (
 	_ "github.com/captncraig/caddy-realip"                  // realip
-	_ "github.com/captncraig/cors"                          // cors
+	_ "github.com/captncraig/cors/caddy"                    // cors
 	_ "github.com/epicagency/caddy-expires"                 // expires
 	_ "github.com/lucaslorentz/caddy-supervisor/httpplugin" // supervisor
 	_ "github.com/miekg/caddy-prometheus"                   // http.prometheus
